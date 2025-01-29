2535c663e68b7f626b698bc3d8e027db1225496c
Alejandro Gonzalez <alejandro.gonzalez@colostate.edu>
2025-01-29 21:50:14 +0000

    createMessage method in MessageService complete

--- src/main/java/Service/MessageService.java
+++ src/main/java/Service/MessageService.java
@@ -1,5 +1,25 @@
 package Service;
 
+import DAO.MessageDAO;
+import Model.Message;
+
+import java.util.List;
+import java.util.Optional;
+
 public class MessageService {
+    private final MessageDAO messageDAO;
+
+    public MessageService() {
+        this.messageDAO = new MessageDAO();
+    }
+
+
+    // Create message
+    public Message createMessage(Message message) {
+        if(message.getMessage_text().isBlank() || message.getMessage_text().length() > 255) {
+            return null;
+        }
+        return messageDAO.createMessage(message);
+    }
     
 }

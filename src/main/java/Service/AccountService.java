03d9d312d8f24eba506419d70cbc67c41ba7c0cb
Alejandro Gonzalez <alejandro.gonzalez@colostate.edu>
2025-01-29 20:44:08 +0000

    AccountService Class complete

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,12 +1,45 @@
 package Service;
 
+import DAO.AccountDAO;
 import Model.Account;
-import Model.Message;
 
-import static org.junit.Assert.assertArrayEquals;
 
+import java.util.Optional;
 import java.util.List;
 
 public class AccountService {
-    
+    private final AccountDAO accountDAO;
+
+    public AccountService() {
+        this.accountDAO = new AccountDAO();
+    }
+
+
+    // Register new account
+    public Optional<Account> register(Account account) {
+        // Verify username is not null or empty, and password is not null and > 4
+        if (account.getUsername() == null || account.getUsername().isBlank() ||
+            account.getPassword() == null || account.getPassword().length() < 4) {
+                return Optional.empty();
+        }
+
+        // Check if username is already taken
+        if(accountDAO.getAccountByUsername(account.getUsername()).isPresent()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(accountDAO.createAccount(account));
+    }
+
+
+    // Authenticate user by checking if username and password match a stored account
+    public Optional<Account> login(String username, String password) {
+        Optional<Account> existingAccount = account(DAO.getAccountByUsername(username));
+
+        if(existingAccount.isPresent() && existingAccount.get().getPassword().equals(password)) {
+            return existingAccount;
+        }
+
+        return Optional.empty();
+    }
 }
